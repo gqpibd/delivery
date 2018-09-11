abe09f908df7830e618552b44990b51afccdc955
gqpibd <gqpibd@gamil.com>
2018-09-12 01:06:23 +0900

    dh

---
+++ delivery_server/src/dto/OrderBBsDto.java
@@ -0,0 +1,26 @@
+package dto;
+
+public class OrderBBsDto extends OrderDto {
+	
+	
+	public static final int REQUEST = 1;
+	public static final int ONGOING = 2;
+	public static final int DONE = 3;
+	public static final int CANCELED = 4;
+	
+	private String contents;
+	private int price;
+	private String[] applicants;
+	private int status;
+	
+	public OrderBBsDto(int reqNum, String title, String type, String location, String consumerId, String delivererId,
+			String date) {
+		super(reqNum, title, type, location, consumerId, delivererId, date);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public OrderBBsDto(OrderDto oDto) {
+		super(oDto);
+		// TODO Auto-generated constructor stub
+	}	
+}
